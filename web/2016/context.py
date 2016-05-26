041be7c1bd464f2dfd85ec6a86a61c46f5cc173a
Istvan Albert <istvan.albert@gmail.com>
2016-05-26 16:05:26 -0400

    added frames for each day

--- web/2016/context.py
+++ web/2016/context.py
@@ -2,8 +2,8 @@
 navbar = [
     ("Home", "/index.html"),
     ("Day 1", "/day1/day1_schedule.html"),
-    ("Day 2", "/day1/day1_schedule.html"),
-    ("Day 3", "/day1/day1_schedule.html"),
-    ("Day 4", "/day1/day1_schedule.html"),
-    ("Day 5", "/day1/day1_schedule.html"),
+    ("Day 2", "/day2/day2_schedule.html"),
+    ("Day 3", "/day3/day3_schedule.html"),
+    ("Day 4", "/day4/day4_schedule.html"),
+    ("Day 5", "/day5/day5_schedule.html"),
 ]
